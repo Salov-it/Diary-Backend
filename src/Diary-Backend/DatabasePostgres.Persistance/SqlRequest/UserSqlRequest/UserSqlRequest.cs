d123e09d8aad2b331d090fb27dd379c5d0f53657
Salov-it <salovit96@gmail.com>
2023-10-19 18:22:07 +0300

    Регистрация пользователя создана

--- src/Diary-Backend/DatabasePostgres.Persistance/SqlRequest/UserSqlRequest/UserSqlRequest.cs
+++ src/Diary-Backend/DatabasePostgres.Persistance/SqlRequest/UserSqlRequest/UserSqlRequest.cs
@@ -4,12 +4,12 @@ namespace DatabasePostgres.Persistance.SqlRequest.UserSqlRequest
 {
     public class UserSqlRequest
     {
-       public string CreateUserTable = "CREATE TABLE Users (id int PRIMARY KEY, Login TEXT UNIQUE, Password TEXT, Phone TEXT, Created DATE);";
+       public string CreateUserTable = "CREATE TABLE Users (id SERIAL PRIMARY KEY, Login TEXT UNIQUE, Password TEXT, Phone TEXT, Created DATE);";
       
        public string DeleteUserTable = "DROP TABLE Users;";
 
         public string UserAdd = $"INSERT INTO Users (login,password,Phone,Created)" +
-             "VALUES (@Login,@Password,@Phone,@Created))";
+             "VALUES (@Login,@Password,@Phone,@Created)";
 
         public string GetAll = "SELECT Login,Passwords,Phone,Created FROM Users;";
     }
