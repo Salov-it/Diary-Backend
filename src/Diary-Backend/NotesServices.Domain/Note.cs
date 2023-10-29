abdb4957a91121a8eb5af413ae6fed1655fa845e
salov-it <salovit96@gmail.com>
2023-10-29 18:03:24 +0300

    ref

---
+++ src/Diary-Backend/NotesServices.Domain/Note.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace NotesServices.Domain
+{
+    public class Note
+    {
+        public int id { get; set; }
+        public string NickId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
