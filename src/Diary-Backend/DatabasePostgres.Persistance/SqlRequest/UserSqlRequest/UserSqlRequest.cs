9599f8394f7f4000e3c211a84290b2aad94302d6
Salov-it <salovit96@gmail.com>
2023-12-23 22:13:49 +0300

    ref sql UserAdd

--- src/Diary-Backend/DatabasePostgres.Persistance/SqlRequest/UserSqlRequest/UserSqlRequest.cs
+++ src/Diary-Backend/DatabasePostgres.Persistance/SqlRequest/UserSqlRequest/UserSqlRequest.cs
@@ -7,7 +7,7 @@ namespace DatabasePostgres.Persistance.SqlRequest.UserSqlRequest
        public string CreateUserTable = "CREATE TABLE Users (id SERIAL PRIMARY KEY, Login TEXT UNIQUE, Password TEXT, Phone TEXT,Role TEXT, Created DATE);";
 
         public string UserAdd = $"INSERT INTO Users (login,password,Phone,Role,Created)" +
-             "VALUES (@Login,@Password,@Phone,@Role,@Created)";
+             "VALUES ($1,$2,$3,$4,$5)";
 
         public string UserUpdate = "UPDATE Users SET phone = @Phone WHERE login = @Login;";
 
